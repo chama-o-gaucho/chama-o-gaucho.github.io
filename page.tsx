a3df759a7318bcb6cd90715554b984b34380c292
chama-o-gaucho <149335680+chama-o-gaucho@users.noreply.github.com>
2023-11-01 15:51:03 -0300

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,21 +1,13 @@
 'use client';
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import Image from 'next/image';
-import Item from '@mui/material/Stack';
-import { GGallery } from '../components/GGallery';
 import { useState } from 'react';
 import { Dialog, DialogTitle, DialogContent } from '@mui/material';
 
