6b59a81a3bf5d9920acc594857943f729d32f307
chama-o-gaucho <149335680+chama-o-gaucho@users.noreply.github.com>
2023-11-01 16:30:00 -0300

    Add files via upload

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -25,21 +25,99 @@ export default function Album() {
   const cards: Card[] = [
     {
       id: 1,
-      image: '/gallery/2.jpg',
+      image: '/gallery/1.jpg',
       title: 'Título 1',
       description: 'Descrição 1.',
     },
     {
       id: 2,
-      image: '/gallery/3.jpg',
+      image: '/gallery/2.jpg',
       title: 'Título 2',
       description: 'Descrição 2.',
     },
     {
       id: 3,
-      image: '/gallery/4.jpg',
+      image: '/gallery/3.jpg',
       title: 'Título 3',
       description: 'Descrição 3.',
+    },
+    {
+      id: 4,
+      image: '/gallery/4.jpg',
+      title: 'Título 4',
+      description: 'Descrição 4.',
+    },
+    {
+      id: 5,
+      image: '/gallery/5.jpg',
+      title: 'Título 5',
+      description: 'Descrição 5.',
+    },
+    {
+      id: 3,
+      image: '/gallery/6.jpg',
+      title: 'Título 6',
+      description: 'Descrição 6.',
+    },
+    {
+      id: 3,
+      image: '/gallery/7.jpg',
+      title: 'Título 7',
+      description: 'Descrição 7.',
+    },
+    {
+      id: 3,
+      image: '/gallery/8.jpg',
+      title: 'Título 8',
+      description: 'Descrição 8.',
+    },
+    {
+      id: 3,
+      image: '/gallery/9.jpg',
+      title: 'Título 9',
+      description: 'Descrição 9.',
+    },
+    {
+      id: 3,
+      image: '/gallery/10.jpg',
+      title: 'Título 10',
+      description: 'Descrição 10.',
+    },
+    {
+      id: 3,
+      image: '/gallery/11.jpg',
+      title: 'Título 11',
+      description: 'Descrição 11.',
+    },
+    {
+      id: 3,
+      image: '/gallery/12.jpg',
+      title: 'Título 12',
+      description: 'Descrição 12.',
+    },
+    {
+      id: 3,
+      image: '/gallery/13.jpg',
+      title: 'Título 13',
+      description: 'Descrição 13.',
+    },
+    {
+      id: 3,
+      image: '/gallery/14.jpg',
+      title: 'Título 14',
+      description: 'Descrição 14.',
+    },
+    {
+      id: 3,
+      image: '/gallery/15.jpg',
+      title: 'Título 15',
+      description: 'Descrição 15.',
+    },
+    {
+      id: 3,
+      image: '/gallery/16.jpg',
+      title: 'Título 16',
+      description: 'Descrição 16.',
     },
     // Adicione mais objetos de card conforme necessário
   ];
@@ -63,7 +141,7 @@ export default function Album() {
           <Container maxWidth="sm">
 
             <Typography className='pt-10 text-center' variant="h5" paragraph>
-              descrição sobre
+              Esta página tem como objetivo mostrar o trabalho do renomado Chef Cozinheiro Cassiano Bilhalva, onde o mesmo vai apresentar a culinária gaúcha, organizar eventos e marcar aulas de culinária. Qualquer dúvida sobre, dê uma olhada na aba contatos ou visualize o Whatsapp e Instagram no final da página.
             </Typography>
 
           </Container>
